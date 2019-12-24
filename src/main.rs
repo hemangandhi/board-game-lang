99e55b5fd67467dc513ac522ccc549acae903b3d
Heman Gandhi <hemangandhi@gmail.com>
2019-12-24 15:36:19 -0500

    start parser

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use crate::parser;
+
 fn main() {
     println!("Hello, world!");
 }
---
+++ src/parser.rs
@@ -0,0 +1,8 @@
+#[macro_use]
+extern crate nom;
+
+pub enum BoardGameSpec {
+
+}
+
+pub fn parse_game(in_stream: [u8]) -> BoardGameSpec {}
