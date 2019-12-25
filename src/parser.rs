b8a0ac22bdf3b185e8f5c009910f3fcca89434fe
Heman Gandhi <hemangandhi@gmail.com>
2019-12-25 02:05:29 -0500

    actual parsing with types that serve that

--- src/parser.rs
+++ src/parser.rs
@@ -1,39 +1,123 @@
 #[macro_use]
 extern crate nom;
 
-pub struct TypeName {
-    name: [u8],
-    generic_args: Vec<[u8]>,
+use nom::error::VerboseError;
+use nom::{
+    branch::{alt, switch},
+    bytes::{tag, take_while},
+    character::complete::alphanumeric1 as alnum,
+    combinator::map,
+    multi::separated_list,
+    sequence::{delimited, pair, preceeded, separated_pair, terminated},
+    Err, IResult,
+};
+
+use std::collections::HashMap;
+
+pub enum TypeType {
+    // user-declared type
+    RecordDecl(HashMap<String, Type>),
+    EnumDecl(Vec<String>),
+    // built-ins
+    Number,
+    // This will have to have generics, but that comes later
+    Function,
+    Declaration,
 }
 
-pub enum FieldReference {
-    Recursive([u8], FieldReference),
-    Base([u8]),
+pub struct Type {
+    name: String,
+    contents: TypeType,
+    // if the generics are being bound, the latter
+    // type is the only known part
+    generics: Result<HashMap<String, Type>, Vec<String>>,
 }
 
-pub enum ValueReference {
-    Field(FieldReference),
-    Value([u8]),
+pub enum Value {
+    RecordField(String, Value),
+    Literal(Type),
 }
 
-pub enum MatcherStmt {
-    And(MatcherStmt, MatcherStmt),
-    Or(MatcherStmt, MatcherStmt),
-    Not(MatcherStmt),
-    AtLeast(ValueReference, ValueReference),
-    AtMost(ValueReference, ValueReference),
+pub enum Comparator {
+    GT,
+    LT,
+    EQ,
+    GTE,
+    LTE,
+    NEQ,
 }
 
-pub enum BodyStmt {
-    IfStmt(MatcherStmt, BodyStmt, BodyStmt),
-    ProvisionStmt(BodyStmt),
-    InstantiationStmt(TypeName, Vec<(FieldReference, ValueReference)>),
+pub enum Matcher {
+    And(Matcher, Matcher),
+    Or(Matcher, Matcher),
+    Not(Matcher),
+    Comparison(Value, Value, Comparator),
 }
 
-pub enum BoardGameStatement {
-    RecordDecl(Vec<([u8], TypeName)>),
-    EnumDecl(Vec<[u8]>),
-    ProviderDecl([u8], Vec<([u8], TypeName)>, BodyStmt),
+fn whitespace(i: &[u8]) -> IResult<&[u8], &[u8]> {
+    let chars = " \t\r\n";
+    take_while(move |c| chars.contains(c))(i)
 }
 
-pub fn parse_game(in_stream: [u8]) -> BoardGameSpec {}
+fn parse_type_instantiation(input: &[u8]) -> IResult<&[u8], Type> {
+    map(
+        separated_pair(
+            preceeded(whitespace, alnum),
+            preceeded(whitespace, "of"),
+            separated_list(delimited(whitespace, tag("and"), whitespace), alnum),
+        ),
+        |name, typs| Type {
+            name: name,
+            contents: TypeType::Declaration,
+            generics: Result::Err(typs),
+        },
+    )
+}
+
+fn parse_enum(input: &[u8]) -> IResult<&[u8], TypeType> {
+    map(
+        separated_list(preceeded(whitespace, alt(tag("or a"), tag("or an"))), alnum),
+        |v| TypeType::EnumDecl(v),
+    )
+}
+
+fn parse_record(input: &[u8]) -> IResult<&[u8], TypeType> {
+    map(
+        separated_list(
+            preceeded(whitespace, alt(tag("and a"), tag("and an"))),
+            separated_pair(
+                delimited(whitespace, parse_type_instantiation, whitespace),
+                tag("called"),
+                preceeded(whitespace, alnum),
+            ),
+        ),
+        |v| {
+            TypeType::RecordDecl(
+                v.into_iter()
+                    .map(|(typ, name)| (name, typ))
+                    .collect::<HashMap<String, Type>>(),
+            )
+        },
+    )
+}
+
+fn parse_type_decl(input: &[u8]) -> IResult<&[u8], Type> {
+    map(
+        pair(
+            preceeded(whitespace, parse_type_instantiation),
+            switch!(alt(tag("is a"), tag("has a"), tag("is an"), tag("has an")),
+            "is a" => call!(parse_enum),
+            "is an" => call!(parse_enum),
+            "has a" => call!(parse_record),
+            "has an" => call!(parse_record),
+            ),
+        ),
+        |(typ, recs)| Type {
+            name: typ.name,
+            contents: recs,
+            // TODO: figure out how to reconcile the generics we know in recs with the generics we
+            // forward-declare in the type.
+            generics: typ.generics,
+        },
+    )
+}
