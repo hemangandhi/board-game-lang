e165b7b808600bc42428c510a7cf7a3cf6bcfe66
hemangandhi <hemangandhi@gmail.com>
2020-12-12 22:16:48 -0500

    ng2-mode for emacs in dev deps

--- dev-env.nix
+++ dev-env.nix
@@ -3,12 +3,14 @@ with import <nixpkgs> {};
 stdenv.mkDerivation {
   name = "board-game-lang-env";
   buildInputs = [
-    # editor
+    # rust editor
     emacs26Packages.company
     emacs26Packages.rustic
     emacs26Packages.flycheck
     emacs26Packages.eglot
     emacs26Packages.cargo
+    # ng editor
+    emacs26Packages.ng2-mode
     # rust linter
     clippy
     # rust builder
